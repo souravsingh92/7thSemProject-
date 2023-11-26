afc39851d16941046366c82cb4324b13ca79b691
Sourav Singh <50536628+souravsingh92@users.noreply.github.com>
2023-11-26 19:40:55 +0530

    Add files via upload

---
+++ src/database/index.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const configOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const connectToDB = async () => {
+  const connectionUrl =
+    "mongodb+srv://afsour7:Singhsourav09@7thsemproject.rksy6qe.mongodb.net/";
+
+  mongoose
+    .connect(connectionUrl, configOptions)
+    .then(() => console.log("Ecommerce database connected successfully!"))
+    .catch((err) =>
+      console.log(`Getting Error from DB connection ${err.message}`)
+    );
+};
+
+export default connectToDB;
